6698be1f62fb075a3e99aa3e76cbd26213da5f85
Eugene <kanvoir.work@gmail.com>
2022-04-06 17:27:23 +0300

    add all js from start curs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,91 @@
+"use strict";
+/* // ! Задание переменных и констант
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+const obj = {
+	a: 50,
+};
+console.log(obj);
+obj.a = 10;
+
+console.log(obj);
+(function () {})();
+// ! Типы данных
+let number = 4.5; // Number
+
+console.log(-4 / 0); // -Infinity
+console.log("string" * 7); // NaN
+
+const string = "Vasya"; // String
+
+const booLean = false; // Boolean
+
+console.log(undef); // null
+
+let jija;
+console.log(jija); // undefined
+
+const obj = {
+	name: "Vasya",
+	age: 24,
+	isMarrid: false,
+};
+console.log(obj.isMarrid);
+console.log(obj.name);
+console.log(obj["age"]);
+
+let arr = [23, "lol", {}, [23, "lol", {}, [], "dick"], "dick"];
+console.log(arr[2]);
+// !Взаимодействие с пользователем
+alert("Hi Nigga!");
+const rezQues = confirm("Ты чмо?");
+console.log(rezQues);
+const answer = +prompt("Длинна указательного пальца?(см)", "2");
+console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("Ваше имя?", "");
+answers[1] = prompt("Ваша фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+console.log(typeof answers);
+
+// !Интерполяция (вставлять внутри строки значения переменных)
+const category = "toys";
+const url = "https://someurl.com/";
+console.log(`${url}${category}/5`); // !используем кавычки там, где буква ё
+
+// !Операторы в JS
+// *Конкатинация - при помощи оператора сложения (унарный плюс) преобразование строк в числа, и чисел в строки
+console.log("someString" + " - object"); // *выйдет строка
+console.log(typeof (4 + "5")); // *выйдет строка
+console.log(typeof (4 + +"5")); // *выйдет число
+
+// *Инкремент и декримент (2 формы префиксная и постфиксная)
+let incr = 10,
+	decr = 10;
+// ++incr;
+// decr--;
+console.log(incr++); //выводит значение потом увеличивает
+console.log(--decr); //уменьшает потом выводит измененное значение
+console.log(2 * 5 == "10"); // true сравнение не строгое (не сравниваются типы данных)
+console.log(2 * 5 === "10"); // false сравнение строгое
+// * = - присваивание
+// * == - сравнение только знчений без учета типа данных
+// * === - строгое сравнение знчений с учетом типа данных
+// * && - И - если все правда возвращает правду
+// * || - ИЛИ - хотябы 1 правда возвращает правду
+// * ! - Отрицание - меняет на противоположное
+// * != - Отрицание - меняет на противоположное
+const isCheked = false,
+	isClose = true;
+console.log(isCheked || !isClose);
+ */
+//!Практика 012
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
@@ -20,8 +108,6 @@
 	 }
 
 Проверить, чтобы все работало без ошибок в консоли */
-
-"use strict";
 
 /* const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 console.log(numberOfFilms);
@@ -112,7 +198,7 @@ for (let i = 1; i < 8; i++){
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
-
+/* 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 const personalMovieDB = {
@@ -164,7 +250,7 @@ while (i < 2) {
 }
 console.log(personalMovieDB);
 
-// * Ивана решение
+// ?Ивана решение
 for (let i = 0; i < 2; i++) {
 	const a = prompt("Один из последних просмотренных фильмов?", ""),
 		b = +prompt("На сколько оцените его?", "");
@@ -175,4 +261,5 @@ for (let i = 0; i < 2; i++) {
 	}
 }
 console.log(personalMovieDB);
-//* конец
+// ?конец
+ */
