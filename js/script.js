cc2444286d9bb947df77b30cdf50521634b4d4e5
Eugene <kanvoir.work@gmail.com>
2022-04-14 13:35:35 +0300

    mobile events

--- js/script.js
+++ js/script.js
@@ -991,3 +991,36 @@ btn2.addEventListener("click", funcClick);
 // 	}
 // 	console.log(node);
 // }
+// ! 034 События на мобильных устройствах
+// touchstart
+// touchmove
+// touchend
+// touchenter
+// touchleave
+// touchcancel
+/* 
+window.addEventListener("DOMContentLoaded", () => {
+	const box = document.querySelector("#box");
+
+	box.addEventListener("touchstart", (e) => {
+		e.preventDefault();
+
+		console.log("start");
+	});
+
+	box.addEventListener("touchmove", (e) => {
+		e.preventDefault();
+
+		console.log(e.targetTouches[0].pageX);
+	});
+
+	box.addEventListener("touchend", (e) => {
+		e.preventDefault();
+
+		console.log("end");
+	});
+});
+ */
+// touches
+// targetTouches
+// chengedTouches
